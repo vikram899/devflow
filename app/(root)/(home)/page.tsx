ec1a0eaefb842b09a0ac7506691516379c801daf
vikram899 <vikramdeshmukh899@gmail.com>
2024-08-25 09:05:51 +0530

    Adding icons and added meta data and title

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   console.log("Home from server comp");
-  return <div>Home</div>;
+  return <div className="h1-bold">Home</div>;
 };
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,30 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community driven platform to answer questions and sharing knowledge.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,7 +40,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
